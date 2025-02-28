ee576d7154019cfab09b418d14b68826f0226d00
tastasu <hi.tasutasu@gmail.com>
2025-03-01 00:16:13 +0700

    Create Retreat State.cs
    Tugas 2

---
+++ Assets/Enemy/Retreat State.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetreatState : BaseState
+{
+    public void EnterState(Enemy enemy)
+    {
+        Debug.Log("Start Patrol");
+    }
+    public void UpdateState(Enemy enemy)
+    {
+        if (enemy.Player != null)
+        {
+            enemy.NavMeshAgent.destination = enemy.transform.position - enemy.Player.transform.position;
+        }
+    }
+    public void ExitState(Enemy enemy)
+    {
+        Debug.Log("Stop Patrol");
+    }
+}
