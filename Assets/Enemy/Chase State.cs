05f90a1491287be3f4f50013a36a1f89bfd18d0d
tastasu <hi.tasutasu@gmail.com>
2025-03-01 00:16:41 +0700

    Create Chase State.cs
    Tugas 2

---
+++ Assets/Enemy/Chase State.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : BaseState
+{
+    public void EnterState(Enemy enemy)
+    {
+        Debug.Log("Start Patrol");
+    }
+    public void UpdateState(Enemy enemy)
+    {
+        if (enemy.Player != null) 
+        {
+            enemy.NavMeshAgent.destination = enemy.Player.transform.position;
+
+            if (Vector3.Distance(enemy.transform.position, enemy.Player.transform.position) > enemy.ChaseDistance)
+            {
+                enemy.SwitchState(enemy.PatrolState);
+            }
+        }
+    }
+    public void ExitState(Enemy enemy)
+    {
+        Debug.Log("Stop Patrol");
+    }
+}
